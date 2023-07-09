2821cabbafcc73f1389c850900690347e464b1c1
Randson <orandson@gmail.com>
2023-07-09 11:29:12 +0100

    add more aggregation queries

---
+++ aggregation/weather-observation-station-13.sql
@@ -0,0 +1,7 @@
+-- Weather Observation Station 13
+-- https://www.hackerrank.com/challenges/weather-observation-station-13/problem
+
+select truncate(sum(lat_n), 4)
+from station
+where lat_n > 38.7880
+  and lat_n < 137.2345;
---
+++ aggregation/weather-observation-station-2.sql
@@ -0,0 +1,5 @@
+-- Weather Observation Station 2
+-- https://www.hackerrank.com/challenges/weather-observation-station-2/problem
+
+select round(sum(lat_n), 2), round(sum(long_w), 2)
+from station;
