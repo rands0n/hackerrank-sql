25ad79a298efc46c6b2b29149b05d6f103fb27e0
Randѕon <orandson@gmail.com>
2023-07-12 19:39:38 +0100

    add more aggregation queries

---
+++ aggregation/revising-aggregations-sum.sql
@@ -0,0 +1,6 @@
+-- Revising Aggregations Sum
+-- https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+
+select sum(population)
+from city
+where district = "California";
